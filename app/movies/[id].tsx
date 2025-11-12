1b3ad30405ede588f3aa3b3fd2246d2efc9d6f80
andbudha <andreibartov@gmail.com>
2025-11-12 11:00:51 +0100

    root layout updated/movie-details screen added

---
+++ app/movies/[id].tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const MovieDetails = () => {
+  return (
+    <View style={styles.container}>
+      <Text>MovieDetails</Text>
+    </View>
+  );
+};
+
+export default MovieDetails;
+
+const styles = StyleSheet.create({
+  container: {},
+});
