aaf7792ec13c9fba2b35c121c9d657f062401b6f
andbudha <andreibartov@gmail.com>
2025-11-21 14:15:59 +0100

    both saved & profile pages updated

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,16 +1,13 @@
-import * as React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { icons } from "@/constants/icons";
+import { Image, Text, View } from "react-native";
 
 const Profile = () => {
   return (
-    <View style={styles.container}>
-      <Text>Profile</Text>
+    <View className="bg-primary flex-1 justify-center items-center">
+      <Image source={icons.person} className="size-10 mb-4" />
+      <Text className="text-slate-300">Profile</Text>
     </View>
   );
 };
 
 export default Profile;
-
-const styles = StyleSheet.create({
-  container: {},
-});
